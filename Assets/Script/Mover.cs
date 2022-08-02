27fbe28c3bb87150f1959371fe03b878a7b57161
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-08-02 16:53:11 +0200

    Added walk/run animation

--- Assets/Script/Mover.cs
+++ Assets/Script/Mover.cs
@@ -12,6 +12,7 @@ public class Mover : MonoBehaviour
         {
             MoveToCursor();
         }
+        UpdateAnimation();
     }
     void MoveToCursor()
         {
@@ -22,7 +23,14 @@ public class Mover : MonoBehaviour
             {
                 GetComponent<NavMeshAgent>().destination = hit.point;
             }
+    }
 
+    private void UpdateAnimation()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
 }
        
