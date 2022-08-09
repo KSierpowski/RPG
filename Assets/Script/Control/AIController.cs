d338e64cd887f5a71848a4517c5fea29dc367504
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-08-09 15:14:04 +0200

    Added enemy chase range

---
+++ Assets/Script/Control/AIController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+
+        [SerializeField] float chaseDistance = 5f;
+
+        
+
+        private void Update()
+        {
+           
+           if (DistanceToPlayer() < chaseDistance)
+            {
+                print("enemy is in range");
+            }
+        }
+
+        private float DistanceToPlayer()
+        {
+        
+            {
+                GameObject player = GameObject.FindGameObjectWithTag("Player");
+                return Vector3.Distance(player.transform.position, transform.position);
+            }
+        }
+
+   
+
+
+    }
+}
