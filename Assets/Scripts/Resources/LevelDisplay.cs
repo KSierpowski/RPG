ffcf4a47803e469ba154a1ac350e2b471bc6afe7
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-09-08 11:49:11 +0200

    Added level display

---
+++ Assets/Scripts/Resources/LevelDisplay.cs
@@ -0,0 +1,22 @@
+using RPG.Stats;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Resources
+{
+    public class LevelDisplay : MonoBehaviour
+    {
+        BaseStats baseStats;
+
+        private void Awake()
+        {
+            baseStats = GameObject.FindWithTag("Player").GetComponent<BaseStats>();
+        }
+
+        private void Update()
+        {
+            GetComponent<Text>().text = String.Format("{0:0}", baseStats.GetLevel());
+        }
+    }
+}
